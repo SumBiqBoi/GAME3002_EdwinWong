4029e53a9ba6f20b40ac801097f919c48498dd12
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2025-04-09 01:50:31 -0400

    Added timer and rotated camera

---
+++ EdwinWong_GAME3002_A3/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TMP_Text timeText;
+
+    private float elapsedTime = 0;
+    private int minutes;
+    private int seconds;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        minutes = Mathf.FloorToInt(elapsedTime / 60);
+        seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
