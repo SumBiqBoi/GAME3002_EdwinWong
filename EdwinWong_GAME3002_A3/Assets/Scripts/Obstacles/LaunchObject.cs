9f7f21042585508c362620b328101e113a1b1e1c
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2026-09-01 16:06:32 -0400

    added delay to LaunchObject

--- EdwinWong_GAME3002_A3/Assets/Scripts/Obstacles/LaunchObject.cs
+++ EdwinWong_GAME3002_A3/Assets/Scripts/Obstacles/LaunchObject.cs
@@ -7,7 +7,33 @@ public class LaunchObject : MonoBehaviour
     [SerializeField] GameObject[] objectToLaunch;
     [SerializeField] Transform launchDirection;
     [SerializeField] float launchSpeed = 2;
+    [SerializeField] float delay = 0;
+
+    bool isActivated = false;
+
+    private void Update()
+    {
+        if (isActivated)
+        {
+            if (delay <=  0)
+            {
+                Launch();
+
+                isActivated = false;
+            }
+            else
+            {
+                delay -= Time.deltaTime;
+            }
+        }
+    }
+
     public void ActivateLaunch()
+    {
+        isActivated = true;
+    }
+
+    void Launch()
     {
         foreach (GameObject obj in objectToLaunch)
         {
