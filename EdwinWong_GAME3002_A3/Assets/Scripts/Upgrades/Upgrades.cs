94fdfb4a75ecadc243189a0fdd9daab33c4ec7f1
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2026-09-12 20:37:39 -0400

    Created scripts for shop upgrades

---
+++ EdwinWong_GAME3002_A3/Assets/Scripts/Upgrades/Upgrades.cs
@@ -0,0 +1,106 @@
+public class Upgrades
+{
+    public float currentEnergy;
+    public float maxEnergy;
+
+    public int coinsHeld;
+
+    public float mass;
+    public float acceleration;
+    public float maxSpeed;
+    public float rotateSpeed;
+
+    public float containerStrength;
+
+    private void Init()
+    {
+        currentEnergy = 0;
+        maxEnergy = 0;
+        coinsHeld = 0;
+        mass = 0;
+        acceleration = 0;
+        maxSpeed = 0; 
+        rotateSpeed = 0;
+        containerStrength = 0;
+    }
+
+    public float GetCurrentEnergy()
+    {
+        return currentEnergy;
+    }
+
+    public void SetCurrentEnergy(float value)
+    {
+        currentEnergy += value;
+    }
+
+    public float GetMaxEnergy()
+    { 
+        return maxEnergy; 
+    }
+
+    public void SetMaxEnergy(float value)
+    {
+        maxEnergy += value;
+    }
+
+    public int GetCoinsHeld()
+    {
+        return coinsHeld;
+    }
+
+    public void SetCoinsHeld(int value)
+    {
+        coinsHeld += value;
+    }
+
+    public float GetMass()
+    {
+        return mass;
+    }
+
+    public void SetMass(float value)
+    {
+        mass += value;
+    }
+
+    public float GetAcceleration()
+    {
+        return acceleration;
+    }
+
+    public void SetAcceleration(float value)
+    {
+        acceleration += value;
+    }
+
+    public float GetMaxSpeed()
+    {
+        return maxSpeed;
+    }
+
+    public void SetMaxSpeed(float value)
+    {
+        maxSpeed += value;
+    }
+
+    public float GetRotateSpeed()
+    {
+        return rotateSpeed;
+    }
+
+    public void SetRotateSpeed(float value)
+    {
+        rotateSpeed += value;
+    }
+
+    public float GetContainerStrength()
+    {
+        return containerStrength;
+    }
+
+    public void SetContainerStrength(float value)
+    {
+        containerStrength += value;
+    }
+}
