db7fac314cf6076f1d354acdc4f07805d7097957
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2026-09-14 14:00:05 -0400

    Preparing for shop upgrade

--- EdwinWong_GAME3002_A3/Assets/Scripts/UI/ShopScreen.cs
+++ EdwinWong_GAME3002_A3/Assets/Scripts/UI/ShopScreen.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopScreen : MonoBehaviour
 {
+    [SerializeField] GameObject menuPopUpGO;
+    
     [SerializeField] TMP_Text coinText;
+    [SerializeField] Button sellButton;
+    [SerializeField] Button buyButton;
+    [SerializeField] Button nextButton;
+
+    MenuPopUp menuPopUp;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ EdwinWong_GAME3002_A3/Assets/Scripts/Upgrades/MenuPopUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPopUp : MonoBehaviour
+{
+    public string itemName;
+    public string itemDesc;
+
+    public Button buyButton;
+
+    public void Buy()
+    {
+
+    }
+}
